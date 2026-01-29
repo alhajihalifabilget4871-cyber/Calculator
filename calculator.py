542257209ffb1f3a5430e0bcb109a7ec3e309f21
alhajihalifabilget4871-cyber <alhajihalifabilget4871@gmail.com>
2026-01-30 00:19:04 +0100

    Calculator
    You can calculations

---
+++ calculator.py
@@ -0,0 +1,7 @@
+x = int(input("Shigar da lamba ta farko: "))
+y = int(input("Shigar da lamba ta biyu: "))
+
+print("Jimilla:", x + y)
+print("Ragi:", x - y)
+print("Ninkawa:", x * y)
+print("Rabawa:", x / y)
